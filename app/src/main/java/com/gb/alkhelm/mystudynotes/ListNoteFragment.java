45cae32795727635b9e5ae9d24fc4b939a64911e
Aleksandr Khelmyanov <alkhelmyanov@gmail.com>
2022-11-22 13:45:03 +0300

    Созданы активити для ландшафтного отображения, и два фрагмента ListNoteFragment (список заметок) и NoteFragment (сами заметки)

---
+++ app/src/main/java/com/gb/alkhelm/mystudynotes/ListNoteFragment.java
@@ -0,0 +1,31 @@
+package com.gb.alkhelm.mystudynotes;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ListNoteFragment extends Fragment {
+
+    public static ListNoteFragment newInstance() {
+        ListNoteFragment fragment = new ListNoteFragment();
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_list_note, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
