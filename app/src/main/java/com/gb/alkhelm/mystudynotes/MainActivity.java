ea145bdff7d5deec60136f986ef13a4d86cd9dee
Aleksandr Khelmyanov <alkhelmyanov@gmail.com>
2022-12-03 01:05:55 +0300

    ДЗ урока №8. Создан новый макет activity_main_drawer.xml (с разметкой DrawerLayout) на базе activity_main.xml. Новый макет установлен как запускающий.

--- app/src/main/java/com/gb/alkhelm/mystudynotes/MainActivity.java
+++ app/src/main/java/com/gb/alkhelm/mystudynotes/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_drawer);
 
         if (savedInstanceState == null) { // если активити новая...
             ListNoteFragment listNoteFragment = ListNoteFragment.newInstance();   // создаем новый фрагмент
