a5c4e9fd487cc41d866ccdaa158f43939aa96843
Aleksandr Khelmyanov <alkhelmyanov@gmail.com>
2022-12-06 22:21:56 +0300

    ДЗ урока №10. Создали RecycleView адаптер. fragment_list_note переделаи под RecycleView. В activity_main прописали логику для работы с RecycleView.

---
+++ app/src/main/java/com/gb/alkhelm/mystudynotes/ListNoteAdapter.java
@@ -0,0 +1,52 @@
+package com.gb.alkhelm.mystudynotes;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ListNoteAdapter extends RecyclerView.Adapter<ListNoteAdapter.MyViewHolder> {
+
+    private String[] data;
+
+    @NonNull
+    @Override // Создает макет ViewHolder (9 штук)
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) { // parent - контейнер в который будем инфлейтить наши макеты, viewType - в зависимости от него будут разные контейнеры (ViewHolder)
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        return new MyViewHolder(layoutInflater.inflate(R.layout.fragment_list_note_item, parent, false));
+    }
+
+    @Override // Связывает макет с View (переиспользует ViewHolder созданный выше)
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        holder.bindContentWithLayout(data[position]); // holder свяжи свой макет с контентом data на позиции position
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.length;
+    }
+
+    public void setData(String[] data) {
+        this.data = data;
+        notifyDataSetChanged(); // перерисовать все полученные данные (очень ресурсоемкий)
+    }
+
+    //Прописываем свой ViewHolder
+    class MyViewHolder extends RecyclerView.ViewHolder {
+        private TextView textView;
+
+        //Макет нашего fragment_list_note_item
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textView = (TextView) itemView;
+        }
+
+        //Метод связывающий контент с макетом  fragment_list_note_item
+        public void bindContentWithLayout(String content) {
+            textView.setText(content);
+        }
+    }
+}
--- app/src/main/java/com/gb/alkhelm/mystudynotes/ListNoteFragment.java
+++ app/src/main/java/com/gb/alkhelm/mystudynotes/ListNoteFragment.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Parcelable;
 import android.view.LayoutInflater;
@@ -13,9 +15,11 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class ListNoteFragment extends Fragment {
 
+    ListNoteAdapter listNoteAdapter;
 
     public static ListNoteFragment newInstance() {
         ListNoteFragment fragment = new ListNoteFragment();
@@ -33,23 +37,65 @@ public class ListNoteFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        initView((LinearLayout) view);
+
+        initAdapter();
+        initRecycler(view);
+
+    }
+
+    // Работаем с RecycleView который позволяет ПЕРЕиспользовать его элементы
+
+    //Инициализируем Адаптер
+    void initAdapter() {
+        // 1. Создали пустой объект адаптера
+        listNoteAdapter = new ListNoteAdapter();
+        // 2. Передаем занчение в адаптер
+        listNoteAdapter.setData(getData());
+    }
+
+    // Иницализируем Recycle
+    void initRecycler(View view) {
+        // Ищем наш RecycleView
+        RecyclerView recyclerView = view.findViewById(R.id.recycleView);
+        // Создаем объект Layout менеджера
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
+        // Связывается со своим менеджером который объясняет как отображать на экране
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setHasFixedSize(true); // если все ViewType одинаковые (моносписок) ускорит процесс надувания
+        // Связываем свой RecycleView c адаптером который объясняет что именно ему нужно выводить
+        recyclerView.setAdapter(listNoteAdapter);
+
+    }
+
+    // Метод для получения нашего списка
+    String[] getData() {
+        String[] data = getResources().getStringArray(R.array.listOfNoteArray);
+        return data;
     }
 
 
-    private void initView(LinearLayout view) {
-        String[] listNote = getResources().getStringArray(R.array.listOfNoteArray); // Создали массив строк, для него вызываем получить ресурсы.получить массив строк (R.array.имя)
+    //  Старый код без RecycleView
 
+    /*private void initView(LinearLayout view) {
+        //LinearLayout linearLayout = (LinearLayout) view; // переменная с родительским контейнером
+        LayoutInflater layoutInflater = getLayoutInflater(); // создан инфлейтер для этого нового фрагмента
+
+        String[] listNote = getResources().getStringArray(R.array.listOfNoteArray); // Создали массив строк, для него вызываем получить ресурсы.получить массив строк (R.array.имя)
         for (int i = 1; i < listNote.length; i++) { // i = 1, чтобы дефолтное значение не отображалось в списке
             String listNoteName = listNote[i];
-            TextView textView = new TextView(getContext()); // создали новый textView привязанный к контексту нашего активити
+
+
+            View listItem = layoutInflater.inflate(R.layout.fragment_list_note_item, view, false); // надуй по шаблону fragment_list_note_item, в контейнер linearLayout (view) объект которого мы создали выше
+            Toast.makeText(getContext(), "номер" + i, Toast.LENGTH_SHORT).show();
+            view.addView(listItem);
+
+            TextView textView = listItem.findViewById(R.id.textView); // создали новый textView привязанный к контексту нашего активити
             textView.setTextSize(30f);
             textView.setGravity(1);
             textView.setText(listNoteName);
-            view.addView(textView);
 
             final int finalI = i;
-            textView.setOnClickListener(new View.OnClickListener() {
+          textView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
                     Note note = new Note(finalI);
@@ -62,7 +108,7 @@ public class ListNoteFragment extends Fragment {
             });
 
         }
-    }
+    }*/
 
     private void showPort(Note note) {
         NoteFragment noteFragment = NoteFragment.newInstance(note);
